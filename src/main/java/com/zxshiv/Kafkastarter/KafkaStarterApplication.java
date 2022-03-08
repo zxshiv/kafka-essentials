a06feb603164cf6aa1cd10a56e6290d3f793dcdb
zxshiv <3xs1996@gmail.com>
2022-03-08 17:46:01 +0530

    Codes changed

---
+++ src/main/java/com/zxshiv/Kafkastarter/KafkaStarterApplication.java
@@ -0,0 +1,13 @@
+package com.zxshiv.Kafkastarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaStarterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zxshiv/Kafkastarter/KafkaStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zxshiv.Kafkastarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
